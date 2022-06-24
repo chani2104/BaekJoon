b75d9060c79dac799e62d7b987d91e6cacfc9279
jongchanim <99734417+chani2104@users.noreply.github.com>
2022-06-24 17:21:32 +0900

    백준 #15649 - N과 M (1)

---
+++ 백준_15649/Main.java
@@ -0,0 +1,41 @@
+package 백준_15649;
+
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.BufferedReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    static int n, m;
+    static int [] arr;
+    static boolean [] visit;
+    public static void main(String[] args) throws IOException {
+        BufferedReader rd = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer str = new StringTokenizer(rd.readLine(), " ");
+
+        n = Integer.parseInt(str.nextToken());
+        m = Integer.parseInt(str.nextToken());
+        arr = new int [m + 1];
+        visit = new boolean [n + 1];
+
+        backTracking(0);
+    }
+
+    static void backTracking(int count) {   // 몇 번째 숫자인지 Count
+        if (count == m) {
+            for (int i = 0; i < m; i++)
+                System.out.print(arr[i] + " ");
+            System.out.println();
+            return;
+        }
+
+        for (int i = 1; i <= n; i++) {
+            if (!visit[i]) {
+                arr[count] = i;
+                visit[i] = true;
+                backTracking(count + 1);
+                visit[i] = false;
+            }
+        }
+    }
+}
